e6a5a3d6b398ea1e9bf473c94577f4d5b39b5d90
Jyoti Prakash <jyotiprakash0737@gmail.com>
2024-02-09 14:00:57 +0530

    initial-render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import QuoteBox from "./component/QuoteBox";
 
-function App() {
+const App = () => {
+  const [quote, setQuote] = useState({ text: "", email: "" });
+  const [genrateColor, setColor] = useState("#000000"); // Initial color
+
+  useEffect(() => {
+    fetchQuote();
+    handleClick();
+  }, []);
+
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/comments"
+      );
+      const data = await response.json();
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomData = data[randomIndex];
+      setQuote({ text: randomData.body, email: randomData.email });
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+    }
+  };
+
+  const handleClick = () => {
+    fetchQuote();
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setColor(randomColor);
+    document.body.style.backgroundColor = randomColor;
+    document.body.style.color = randomColor;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <QuoteBox
+        text={quote.text}
+        email={quote.email}
+        handleClick={handleClick}
+        RandomColor={genrateColor}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/component/QuoteBox.js
@@ -0,0 +1,49 @@
+import "./quotes.css";
+const QuoteBox = ({ text, email, handleClick, RandomColor }) => {
+  return (
+    <>
+      <div id="quote-box" style={{}}>
+        <div className="quote-text" style={{ opacity: 1 }}>
+          <i className="fa fa-quote-left"> </i>
+          <span id="text" className="comment">
+            {text}
+          </span>
+        </div>
+        <div className="quote-author" style={{ opacity: 1 }}>
+          - <span id="author">{email.split("@")[0]}</span>
+        </div>
+        <div className="buttons">
+          <a
+            className="button"
+            id="tweet-quote"
+            title="Tweet this quote!"
+            target="_top"
+            href="https://www.linkedin.com/in/jyoti-prakash-8287151bb/"
+            style={{ backgroundColor: RandomColor }}
+          >
+            <i className="fa fa-linkedin" />
+          </a>
+          <a
+            className="button"
+            id="tumblr-quote"
+            title="Post this quote on tumblr!"
+            target="_blank"
+            href="https://github.com/JyotiPrakash95"
+            style={{ backgroundColor: RandomColor }}
+          >
+            <i className="fa fa-github" />
+          </a>
+          <button
+            className="button"
+            id="new-quote"
+            style={{ backgroundColor: RandomColor }}
+            onClick={handleClick}
+          >
+            New quote
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default QuoteBox;
